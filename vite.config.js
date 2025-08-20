1199cf8550b132ca30d8f2885fc01e2b3b1bec12
Vijay-Psgtech <rvk.its@psgtech.ac.in>
2025-08-20 11:43:01 +0530

    vite config allowedHosts added.

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: ["psgsonscharities.org", "localhost"],
+    allowedHosts: ["psgsonscharities.org", "www.psgsonscharities.org", "localhost"],
   },
 });
